99c5cbe835083b9df9de647a14ea9170ef1f76db
Nate Swenson <swenson@umd.edu>
2017-09-21 12:11:55 -0400

    Class 4 second commit

---
+++ Class.4/term.R
@@ -0,0 +1,12 @@
+#My terminal attempt
+
+tmp = runif(50)
+
+for(i in 1:10){
+
+samp = sample(tmp, 10, replace=T)
+
+write.table(as.matrix(samp), paste(i, ".output.txt", sep=""), sep="\t",row.names=F,col.names=F,quote=F)
+
+
+}
